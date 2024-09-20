1c7258cc62c3ebd394a614b58c05d338ad758094
huang.hanson/黄忠_沪_HQDV <hanson.huang@51job.com>
2024-09-20 19:57:44 +0800

    feat:搭建骨架

---
+++ scs-main/src/test/java/com/smart/classroom/subscription/ScsMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smart.classroom.subscription;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScsMainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
